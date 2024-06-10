5c5cf9259e8e6f8f733f86292e141a717bb75476
vova vasylenko <pozilaaskymbria@gmail.com>
2024-06-10 12:05:51 +0300

    Add Heading and Button

---
+++ src/components/Heading/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+import Button from "../Button";
+
+
+const Heading = () => {
+    return (
+        <h1 title='qwerty' className='heading'>
+            Hi!
+            <Button content="!!!" number="111"/>
+            <em className='inside'>
+                Qwerty
+            </em>
+        </h1>
+    );
+}
+export default Heading;
